86155b41217e090444d9bc8768f2d7a86df044a9
nutty <nuttylmao@gmail.com>
2021-01-07 03:41:42 +1100

    Added an uninstaller

---
+++ Uninstaller.bat
@@ -0,0 +1,5 @@
+del /f "C:\Windows\System32\OBSCommand.exe"
+del /f "C:\Windows\System32\Newtonsoft.Json.dll"
+del /f "C:\Windows\System32\obs-websocket-dotnet.dll"
+del /f "C:\Windows\System32\websocket-sharp.dll"
+pause

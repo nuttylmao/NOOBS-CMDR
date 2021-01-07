c8c597b76e9b1c1535dd24160164acf4d2c2f175
nutty <nuttylmao@gmail.com>
2021-01-08 04:12:27 +1100

    Updated 'Installer.bat' and 'Uninstaller.bat'

--- Installer.bat
+++ Installer.bat
@@ -1,2 +1,4 @@
 xcopy "%~dp0OBSCommand" C:\Windows\System32 /y
+xcopy "%~dp0OBSCommand" C:\Windows\System32 /y
+xcopy "%~dp0OBSCommand" C:\Windows\SysWOW64 /y
 pause
--- Uninstaller.bat
+++ Uninstaller.bat
@@ -2,4 +2,8 @@ del /f "C:\Windows\System32\OBSCommand.exe"
 del /f "C:\Windows\System32\Newtonsoft.Json.dll"
 del /f "C:\Windows\System32\obs-websocket-dotnet.dll"
 del /f "C:\Windows\System32\websocket-sharp.dll"
+del /f "C:\Windows\SysWOW64\OBSCommand.exe"
+del /f "C:\Windows\SysWOW64\Newtonsoft.Json.dll"
+del /f "C:\Windows\SysWOW64\obs-websocket-dotnet.dll"
+del /f "C:\Windows\SysWOW64\websocket-sharp.dll"
 pause
